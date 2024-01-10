dd15888af86786b1c412b63b1c2cfb6b7448f667
Matthew Proteau <matthew.proteau@gmail.com>
2024-01-10 15:43:58 -0800

    delete authorization and authentication edit index

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const passport = require('passport');
 require('./passport');
 
 const Movies = Models.Movie;
-const Users = Models.Users;
+const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlix', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/myFlix');
 
 app.use(bodyParser.json());
 
@@ -23,8 +23,8 @@ app.get('/', (request, response)=>{ //request route
     response.send('Hello!') //response
 });  
 
-//Read movies
-app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, res) => {  
+//Read ALL movies
+app.get('/movies', async (req, res) => {  
     Movies.find()  //read all movies
       .then((movies) => {res.status(201).json(movies)})
       .catch((err) => {
@@ -33,10 +33,35 @@ app.get('/movies', passport.authenticate('jwt', {session: false}), async (req, r
       });
 });
 
+//Read data about movie by title 
+app.get('/movies/:title',  (req, res) => {
+   Movies.findOne({ title: req.params.title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 //Read users
-app.get('/users', passport.authenticate('jwt', {session: false}), async (req, res) => {  
+app.get('/users', async(req, res) => {  
   Users.find()  //read all users
-    .then((movies) => {res.status(201).json(users)})
+    .then((users) => {res.status(201).json(users)})
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', async(req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+      console.log(user);
+    })
     .catch((err) => {
       console.error(err);
       res.status(500).send('Error: ' + err);
@@ -44,31 +69,31 @@ app.get('/users', passport.authenticate('jwt', {session: false}), async (req, re
 });
 
 //Create new user
-app.post('/users', passport.authenticate('jwt', {session: false}), async (req, res) => {
-  await Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-      } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
-});
+// app.post('/users', passport.authenticate('jwt', {session: false}), async (req, res) => {
+//   await Users.findOne({ Username: req.body.Username })
+//     .then((user) => {
+//       if (user) {
+//         return res.status(400).send(req.body.Username + 'already exists');
+//       } else {
+//         Users
+//           .create({
+//             Username: req.body.Username,
+//             Password: req.body.Password,
+//             Email: req.body.Email,
+//             Birthday: req.body.Birthday
+//           })
+//           .then((user) =>{res.status(201).json(user) })
+//         .catch((error) => {
+//           console.error(error);
+//           res.status(500).send('Error: ' + error);
+//         })
+//       }
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).send('Error: ' + error);
+//     });
+// });
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080');
