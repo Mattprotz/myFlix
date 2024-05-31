494c19142e613440f52ea05b087351c8f44d2ae2
Mattprotz <matthew.proteau@gmail.com>
2024-05-31 11:40:20 -0700

    Update allowed origins

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let auth = require("./auth")(app);
-let allowedOrigins = ["http://localhost:8080", "http://localhost:1234", "https://netfixmovies.netlify.app'" ];
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://localhost:1234",
+  "https://netfixmovies.netlify.app",
+  "localhost:4200",
+];
 
 const passport = require("passport");
 require("./passport");
